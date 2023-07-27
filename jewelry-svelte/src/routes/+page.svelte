435b6c3d690fb43eecf3c2058865cce284500c5f
tarados62 <sergejzvada62@gmail.com>
2023-07-27 15:22:25 +0300

    to home

--- jewelry-svelte/src/routes/+page.svelte
+++ jewelry-svelte/src/routes/+page.svelte
@@ -1,12 +1,20 @@
 <script>
-	/* import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png'; */
+	import { Hamburger } from 'svelte-hamburgers';
+	import Menu from './Menu.svelte';
+
+	/**
+	 * @type {any}
+	 */
+	let open;
 </script>
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
+<Hamburger  bind:open
+	--color="black" />
+<Menu bind:open />
 
 <style>
 </style>
