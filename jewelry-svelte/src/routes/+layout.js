5a85401693154a054f781c1354f2635cb81f866d
tarados <lith2009@mail.ru>
2023-08-06 15:18:49 +0300

    create locales for Langbox

--- jewelry-svelte/src/routes/+layout.js
+++ jewelry-svelte/src/routes/+layout.js
@@ -2,7 +2,14 @@
 /** @type {import('./$types').LayoutData} */
 export async function load() {
 	const response = await fetch('https://alloe-vega-app.party.space/app/locales');
-	const locales = await response.json();
+	const data = await response.json();
+	let keyLocales = Object.keys(data);
+	let locales = [];
+	for (let i = 0; i < keyLocales.length; i++) {
+		let locale = {};
+		locale[keyLocales[i].slice(3)] = data[keyLocales[i]];
+		locales.push(locale);
+	}
 	return {
 		links: [
 			{ route: '/', title: 'MenuHome' },
