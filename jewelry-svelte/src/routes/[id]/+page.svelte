87d8efbe536d9562c55d35d3368bfc67d58e9f0f
tarados <lith2009@mail.ru>
2023-08-21 13:50:47 +0300

    before install skeleton

--- jewelry-svelte/src/routes/[id]/+page.svelte
+++ jewelry-svelte/src/routes/[id]/+page.svelte
@@ -1,5 +1,9 @@
 <script>
+	import Product from '$lib/Product.svelte';
+
 	export let data;
+	console.log(data.product);
 </script>
 
 <h1>page id {data.product.id}</h1>
+<!-- <Product productData={data.product} /> -->
