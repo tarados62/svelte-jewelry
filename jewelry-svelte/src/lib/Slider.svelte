1e452d8f14fbdab822e88ddcfa9c67d8b093cb51
tarados62 <sergejzvada62@gmail.com>
2023-09-28 15:38:49 +0300

    create component Slider

---
+++ jewelry-svelte/src/lib/Slider.svelte
@@ -0,0 +1,90 @@
+<script>
+// @ts-nocheck
+
+    export const sliders = [
+        "https://alloe-vega-app.party.space//media/cache/cf/5e/cf5e1dd618d82d4c8d166a58a8f083b6.jpg",
+        "https://alloe-vega-app.party.space//media/cache/55/c5/55c5a8359a7c1679bff6729cd10ff4d4.jpg",
+        "https://alloe-vega-app.party.space//media/cache/b9/e4/b9e49ad1e14d73a91456e15e32b85987.jpg",
+        "https://alloe-vega-app.party.space//media/cache/9b/99/9b994ad204fef801cb499a3c8723dcb6.jpg",
+        "https://alloe-vega-app.party.space//media/cache/f8/c1/f8c1ad93e41bc021553d62b13914dc78.jpg",
+        "https://alloe-vega-app.party.space//media/cache/fb/26/fb267a0e1e573fc2c509d86ddda0b479.jpg",
+        "https://alloe-vega-app.party.space//media/cache/b3/0a/b30af82d21ec94508cf19ec9ea8ee0ad.jpg",
+        "https://alloe-vega-app.party.space//media/cache/5f/e6/5fe61b9eb76fd41dda3a4a8f545b6635.jpg"
+ ];
+
+    // @ts-ignore
+    let elemCarousel;
+
+	function carouselLeft() {
+		const x =
+			// @ts-ignore
+			elemCarousel.scrollLeft === 0
+				// @ts-ignore
+				? elemCarousel.clientWidth * elemCarousel.childElementCount // loop
+				// @ts-ignore
+				: elemCarousel.scrollLeft - elemCarousel.clientWidth; // step left
+		// @ts-ignore
+		elemCarousel.scroll(x, 0);
+	}
+
+	function carouselRight() {
+		const x =
+			// @ts-ignore
+			elemCarousel.scrollLeft === elemCarousel.scrollWidth - elemCarousel.clientWidth
+				? 0 // loop
+				// @ts-ignore
+				: elemCarousel.scrollLeft + elemCarousel.clientWidth; // step right
+		// @ts-ignore
+		elemCarousel.scroll(x, 0);
+	}
+
+	// @ts-ignore
+	function carouselThumbnail(index) {
+		// @ts-ignore
+		elemCarousel.scroll(elemCarousel.clientWidth * index, 0);
+	}
+</script>
+
+<div class="grid grid-cols-1 gap-2">
+    <div class="p-4 grid grid-cols-[auto_1fr_auto] gap-2 items-center">
+        <button type="button" class="btn-icon variant-filled" on:click={carouselLeft}>
+            <i class="fa-solid fa-arrow-left" />
+        </button>
+        <div
+            bind:this={elemCarousel}
+            class="snap-x snap-mandatory scroll-smooth flex overflow-x-auto"
+        >
+            {#each sliders as slider}
+                <img
+                    class="snap-center w-[1024px] rounded-container-token"
+                    src={slider}
+                    alt={slider}
+                    loading="lazy"
+                />
+            {/each}
+        </div>
+        <button type="button" class="btn-icon variant-filled" on:click={carouselRight}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                class="w-6 h-6"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+            </svg>
+        </button>
+    </div>
+    <div class="p-4 grid grid-cols-6 gap-1">
+        {#each sliders as slider, i}
+            <button type="button" on:click={() => carouselThumbnail(i)}>
+                <img class="rounded-container-token" src={slider} alt={slider} loading="lazy" />
+            </button>
+        {/each}
+    </div>
+</div>
--- jewelry-svelte/src/routes/[id]/+page.svelte
+++ jewelry-svelte/src/routes/[id]/+page.svelte
@@ -5,85 +5,17 @@
 	// @ts-ignore
 	import { tt } from '$lib/services/stores';
 	import { addToCart } from '$lib/services/cart';
+	import Slider from '$lib/Slider.svelte';
 	// @ts-ignore
 	export let data;
 	let product = data.product;
 	product.quantity = 1;
 
-	let elemCarousel;
-	const unsplashIds = product.image_list;
-
-	function carouselLeft() {
-		const x =
-			elemCarousel.scrollLeft === 0
-				? elemCarousel.clientWidth * elemCarousel.childElementCount // loop
-				: elemCarousel.scrollLeft - elemCarousel.clientWidth; // step left
-		elemCarousel.scroll(x, 0);
-	}
-
-	function carouselRight() {
-		const x =
-			elemCarousel.scrollLeft === elemCarousel.scrollWidth - elemCarousel.clientWidth
-				? 0 // loop
-				: elemCarousel.scrollLeft + elemCarousel.clientWidth; // step right
-		elemCarousel.scroll(x, 0);
-	}
-
-	function carouselThumbnail(index) {
-		elemCarousel.scroll(elemCarousel.clientWidth * index, 0);
-	}
+	
 </script>
 
 <div class="grid grid-cols-2 gap-2">
-	<div class="grid grid-cols-1 gap-2">
-		<div class="p-4 grid grid-cols-[auto_1fr_auto] gap-2 items-center">
-			<!-- Button: Left -->
-			<button type="button" class="btn-icon variant-filled" on:click={carouselLeft}>
-				<i class="fa-solid fa-arrow-left" />
-			</button>
-			<!-- Full Images -->
-			<div
-				bind:this={elemCarousel}
-				class="snap-x snap-mandatory scroll-smooth flex overflow-x-auto"
-			>
-				{#each unsplashIds as unsplashId}
-					<img
-						class="snap-center w-[1024px] rounded-container-token"
-						src={unsplashId}
-						alt={unsplashId}
-						loading="lazy"
-					/>
-				{/each}
-			</div>
-			<!-- Button: Right -->
-			<button type="button" class="btn-icon variant-filled" on:click={carouselRight}>
-				<!-- svelte-ignore invalid-html-attribute -->
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={1.5}
-					stroke="currentColor"
-					className="w-6 h-6"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-					/>
-				</svg>
-
-				<!-- <i class="fa-arrow-right" /> -->
-			</button>
-		</div>
-		<div class="p-4 grid grid-cols-6 gap-1">
-			{#each unsplashIds as unsplashId, i}
-				<button type="button" on:click={() => carouselThumbnail(i)}>
-					<img class="rounded-container-token" src={unsplashId} alt={unsplashId} loading="lazy" />
-				</button>
-			{/each}
-		</div>
-	</div>
+	<Slider />
 
 	<div class="grid grid-cols-1 grid-flow-row grid-rows-[5rem_3rem_3rem_1fr] gap-1">
 		<div class="my-2 mx-0 uppercase tracking-wider text-xl">
