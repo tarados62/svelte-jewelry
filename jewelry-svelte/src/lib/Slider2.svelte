0d4dd1d635702cd6a2c0e06c90be385c08b9def5
tarados62 <sergejzvada62@gmail.com>
2023-10-12 15:26:48 +0300

    to home 1210

--- jewelry-svelte/src/lib/Slider2.svelte
+++ jewelry-svelte/src/lib/Slider2.svelte
@@ -20,52 +20,48 @@
 		currentSlide = index;
 	}
 </script>
-
-<div class="w-full h-full">
-	<div class="flex w-full h-full">
-		<div class="relative h-full w-full">
-			<img
-				class="max-h-[490px] w-full"
-				src={slides[currentSlide]}
-				alt="qq"
-			/>
-			<button
-				type="button"
-				class="absolute top-[40%] left-0 btn-icon bg-initial"
-				on:click={prevSlide}
+<div class="flex flex-nowrap">
+	<div class="relative  w-full">
+		<img
+			class="h-auto w-full"
+			src={slides[currentSlide]}
+			alt="qq"
+		/>
+		<button
+			type="button"
+			class="absolute top-[40%] left-0 btn-icon bg-initial"
+			on:click={prevSlide}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-10 h-10"
+				style="color: black;"
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-10 h-10"
-					style="color: black;"
-				>
-					<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-				</svg>
-			</button>
-			<button
-				type="button"
-				class="absolute top-[40%] right-0 btn-icon bg-initial"
-				on:click={nextSlide}
+				<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+			</svg>
+		</button>
+		<button
+			type="button"
+			class="absolute top-[40%] right-0 btn-icon bg-initial"
+			on:click={nextSlide}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-10 h-10"
+				style="color:black"
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-10 h-10"
-					style="color:black"
-				>
-					<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-				</svg>
-			</button>
-		</div>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+			</svg>
+		</button>
 	</div>
-
 	<div class="p-4 grid grid-cols-6 gap-1">
 		{#each slides as slider, i}
 			<button type="button" on:click={() => carouselThumbnail(i)}>
