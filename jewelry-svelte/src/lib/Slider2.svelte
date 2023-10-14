83fb94c18871db0e4c5a17b0642e00581b3763a0
tarados <lith2009@mail.ru>
2023-10-14 10:42:47 +0300

    add adaptivity for arrow left and right in Slider2

--- jewelry-svelte/src/lib/Slider2.svelte
+++ jewelry-svelte/src/lib/Slider2.svelte
@@ -6,30 +6,26 @@
 	// @ts-ignore
 
 	let currentSlide = 0;
-	
+
 	function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-  }
+		currentSlide = (currentSlide + 1) % slides.length;
+	}
 
-  function prevSlide() {
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  }
-
+	function prevSlide() {
+		currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+	}
 
 	function carouselThumbnail(index) {
 		currentSlide = index;
 	}
 </script>
+
 <div class="flex flex-wrap content-start w-full">
 	<div class="relative w-[calc(100%+200rem)]">
-		<img
-			class="h-auto w-full"
-			src={slides[currentSlide]}
-			alt="qq"
-		/>
+		<img class="h-auto w-full" src={slides[currentSlide]} alt="qq" />
 		<button
 			type="button"
-			class="absolute top-[40%] left-0 btn-icon bg-initial"
+			class="absolute top-[40%] ml-[-15px] sm:m-0 left-0 btn-icon bg-initial"
 			on:click={prevSlide}
 		>
 			<svg
@@ -38,7 +34,7 @@
 				viewBox="0 0 24 24"
 				stroke-width="1.5"
 				stroke="currentColor"
-				class="w-10 h-10"
+				class="w-7 sm:w-10 h-auto"
 				style="color: black;"
 			>
 				<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
@@ -46,7 +42,7 @@
 		</button>
 		<button
 			type="button"
-			class="absolute top-[40%] right-0 btn-icon bg-initial"
+			class="absolute top-[40%] mr-[-15px] sm:m-0 right-0 btn-icon bg-initial"
 			on:click={nextSlide}
 		>
 			<svg
@@ -55,7 +51,7 @@
 				viewBox="0 0 24 24"
 				stroke-width="1.5"
 				stroke="currentColor"
-				class="w-10 h-10"
+				class="w-7 sm:w-10 h-auto"
 				style="color:black"
 			>
 				<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
