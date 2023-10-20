9875b44e1bf9a609767865a218b4d37e42e6a20b
tarados62 <sergejzvada62@gmail.com>
2023-10-20 15:23:57 +0300

    add Checkout.svelte

---
+++ jewelry-svelte/src/lib/Checkout.svelte
@@ -0,0 +1,18 @@
+
+<select class="select">
+	<option value="1">Option 1</option>
+	<option value="2">Option 2</option>
+	<option value="3">Option 3</option>
+	<option value="4">Option 4</option>
+	<option value="5">Option 5</option>
+</select>
+					
+<input class="input" title="Input (text)" type="text" placeholder="input text" />
+<input class="input" title="Input (text)" type="text" placeholder="input text" />
+<input class="input" title="Input (text)" type="text" placeholder="input text" />
+<input class="input" title="Input (text)" type="text" placeholder="input text" />
+<input class="input" title="Input (text)" type="text" placeholder="input text" />
+<input class="input" title="Input (text)" type="text" placeholder="input text" />
+<input class="input" title="Input (text)" type="text" placeholder="input text" />
+<input class="input" title="Input (text)" type="text" placeholder="input text" />
+<textarea class="textarea" rows="4" placeholder="Enter some long form content." />
---
+++ jewelry-svelte/src/routes/checkout/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Checkout from "$lib/Checkout.svelte";
+
+</script>
+
+<Checkout />
