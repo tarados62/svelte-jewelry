c92f1356ffc40ff3492c1470f0e5066b9bb24128
tarados62 <sergejzvada62@gmail.com>
2023-10-27 10:04:19 +0300

    fixed conflict merge

--- jewelry-svelte/src/lib/Input.svelte
+++ jewelry-svelte/src/lib/Input.svelte
@@ -1,33 +1,36 @@
-<script lang="ts">
-	import { InputChip, toastStore } from '@skeletonlabs/skeleton';
+<<script>
 	import { tt } from '$lib/services/stores';
 
-	// export let valueInput;
-
-	function isValidEmail(value: string): boolean {
-		console.log('jjjjj');
-		return value.includes('@') && value.includes('.');
+	export let value = '';
+	export let placeholder = '';
+	/**
+	 * @type {any}
+	 */
+	let variant;
+	/**
+	 *
+	 * @param {any} val
+	 */
+	function isValidEmail(val) {
+		let jsonString = JSON.stringify(val);
+		if (
+			(jsonString.indexOf('@') != -1 && jsonString.indexOf('.') != -1) ||
+			jsonString.indexOf('@') < jsonString.indexOf('.')
+		) {
+			console.log(val);
+			return true;
+		} else {
+			console.log('ii');
+			return false;
+		}
 	}
-
-	function onInvalidHandler(event: any): void {
-		toastStore.trigger({
-			message: `"${event.detail.input}" is an invalid value. Please try again!`,
-			background: 'variant-filled-error'
-		});
-	}
-	let name = '';
+	$: isValidEmail(value) ? (variant = 'success') : (variant = 'error');
 </script>
 
-<!-- <input class="input" title="Input (text)" type="text" placeholder="input text" /> -->
-<InputChip
-	name="chips-example-emails"
-	placeholder="Enter Emails..."
-	chips="variant-filled-secondary"
-	validation={isValidEmail}
+<input
+	class="input-{variant}"
+	type="text"
+	bind:value
+	{placeholder}
+	on:change={() => isValidEmail({ value })}
 />
-<!-- <InputChip ... on:invalid={onInvalidHandler} />
-<InputChip ... max={3} />
-<InputChip ... minlength={2} maxlength={5} /> -->
-
-<!-- <label for="name">{name}</label>
-<input id="name" bind:value={name} /> -->
