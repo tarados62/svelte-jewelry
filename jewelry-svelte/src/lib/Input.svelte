abd3009d3258b503bcef56581574d63d1fcdabb0
tarados62 <sergejzvada62@gmail.com>
2023-10-27 15:48:51 +0300

    to home 2710

--- jewelry-svelte/src/lib/Input.svelte
+++ jewelry-svelte/src/lib/Input.svelte
@@ -1,36 +1,44 @@
-<<script>
+<script>
 	import { tt } from '$lib/services/stores';
 
 	export let value = '';
 	export let placeholder = '';
+	export const required = false;
+	let isValid = true;
+	/**
+	 *
+	 * @param {any} email
+	 */
+	function validateEmail(email) {
+		const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+		console.log(emailRegex.test(email));
+		return emailRegex.test(email);
+	};
+	/**
+	 *
+	 * @param {any} event
+	 */
+	function handleChange(event) {
+		value = event.target.value;
+		isValid = validateEmail(value);
+	}
 	/**
 	 * @type {any}
 	 */
 	let variant;
-	/**
-	 *
-	 * @param {any} val
-	 */
-	function isValidEmail(val) {
-		let jsonString = JSON.stringify(val);
-		if (
-			(jsonString.indexOf('@') != -1 && jsonString.indexOf('.') != -1) ||
-			jsonString.indexOf('@') < jsonString.indexOf('.')
-		) {
-			console.log(val);
-			return true;
-		} else {
-			console.log('ii');
-			return false;
-		}
-	}
-	$: isValidEmail(value) ? (variant = 'success') : (variant = 'error');
+	
+	$: isValid ? value == '' ? (variant = 'warning') : (variant = 'success') : (variant = 'error');
 </script>
 
 <input
+	{required}
 	class="input-{variant}"
-	type="text"
-	bind:value
+	type="email"
+	bind:value={value}
 	{placeholder}
-	on:change={() => isValidEmail({ value })}
+	on:change={handleChange}
 />
+{#if !isValid}
+<p class="error">Enter valid email!</p>
+{/if}
+{value}
