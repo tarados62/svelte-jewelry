0cdb70811642a6558a9a0b8f88def9e03d4c3800
tarados62 <sergejzvada62@gmail.com>
2023-11-02 14:55:08 +0300

    Input.svelte add validation text

--- jewelry-svelte/src/lib/Input.svelte
+++ jewelry-svelte/src/lib/Input.svelte
@@ -1,29 +1,37 @@
 <script>
 // @ts-nocheck
-
-	import { tt } from '$lib/services/stores';
-
 	export let value = '';
 	export let placeholder = '';
 	// @ts-ignore
 	export let required = undefined;
+	export let type = 'text';
 	let isValid = true;
+	const errorStatus = {};
 	/**
 	 *
 	 * @param {any} email
 	 */
 	function validateEmail(email) {
 		const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-		console.log(emailRegex.test(email));
+		if (emailRegex.test(value) == false) {
+			errorStatus.message = 'Enter email';
+		}
 		return emailRegex.test(email);
 	};
+	function validateName() {
+		const textRegex = /^[a-zA-Zа-яА-ЯёЁ]{,25}+$/;
+		if (textRegex.test(value) == false) {
+			errorStatus.message = 'You entered the wrong character';
+		}
+		return textRegex.test(value);
+    };
 	/**
 	 *
 	 * @param {any} event
 	 */
 	function handleChange(event) {
 		value = event.target.value;
-		isValid = validateEmail(value);
+		type == 'email' ? isValid = validateEmail(value) : isValid = validateName(value);
 		if(!isValid) {
 			value = '';
 		}
@@ -43,13 +51,12 @@
 <input
 	{required}
 	class="input input-{variant} pl-2 bg-white rounded-md"
-	bind:value={value}
-	type="text"
+	{value}
+	{type}
 	{placeholder}
 	on:change={handleChange}
 />
 {#if !isValid}
-<span class="text-red-500">Enter valid email!</span>
+<span class="text-red-500">{errorStatus.message}!</span>
 {/if}
-{value}
 
